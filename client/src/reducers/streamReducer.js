fa9b813a53a47802cf32524f783104923517979a
Oleg Lysenok <msk.lysenok@gmail.com>
2019-12-02 13:44:05 +0300

    chore: render streams list

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -10,12 +10,16 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
-    case FETCH_STREAM || CREATE_STREAM || EDIT_STREAM:
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") }; //(247)
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
       return _.omit(state, action.payload); //_.omit - lodash library (245)
-    case FETCH_STREAMS:
-      return { ...state, ..._.mapKeys(action.payload, "id") }; //(247)
     default:
       return state;
   }
